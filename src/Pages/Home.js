58afda2b20409663ac333bc3089c281485d65b63
Akmal Nur Wahid <masakmaltop@gmail.com>
2023-07-03 10:12:24 +0700

    terbaru banggg salma

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,18 +3,22 @@ import Text from '../Components/Text'
 import Heading from '../Components/Heading'
 import Button from '../Components/Button'
 
+
+
 function Home() {
     return (
-      <div className="h-screen overflow-hidden">
+      <div className="h-screen ">
         <Navbar />
         <div className="relative">
           <img src="images/bg4.png" alt="Foto" className="w-full h-auto" />
+          <img src="images/bg3.png" alt="Foto" className="w-full h-auto" />
           <div className="absolute top-0 left-0 right-0 bottom-0">
             <Heading />
             <hr className="border-t-2 border-gray-400" />
             <Text />
             <div className="flex items-center justify-center pt-14">
               <Button />
+    
             </div>
           </div>
         </div>
